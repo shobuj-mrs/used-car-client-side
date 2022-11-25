2a412ad37bb98d8a1e94f4ebd061db2415190c85
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-11-25 21:14:36 +0600

    added a shared componant with loading page

---
+++ src/pages/Shared/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div class="flex justify-center  text-blue-600 items-center">
+            <div class="spinner-border animate-spin inline-block w-16 h-16 border-4 rounded-full" role="status">
+
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
