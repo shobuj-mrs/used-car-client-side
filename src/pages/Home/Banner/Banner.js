47d4d9b87d571a86b49128bcfd6c86fa45ad0952
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-11-26 15:22:22 +0600

    added an another section in my home page

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,42 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const Banner = () => {
     return (
         <section
-  class="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat"
+  className="relative bg-[url(https://images.unsplash.com/photo-1489008777659-ad1fc8e07097?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDZ8fGNhciUyMGJhY2tncm91bmRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60)] bg-cover bg-center bg-no-repeat"
 >
   <div
-    class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/95 sm:to-white/25"
+    className="absolute inset-0 bg-white/50 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/75 sm:to-white/25"
   ></div>
 
   <div
-    class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+    className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
   >
-    <div class="max-w-xl text-center sm:text-left">
-      <h1 class="text-3xl font-extrabold sm:text-5xl">
+    <div className="max-w-xl text-center sm:text-left">
+      <h1 className="text-3xl font-extrabold sm:text-5xl">
         Let us Buy and Sale
 
-        <strong class="block font-extrabold text-rose-700">
+        <strong className="block font-extrabold text-rose-700">
           Your Uses Car.
         </strong>
       </h1>
 
-      <p class="mt-4 max-w-lg sm:text-xl sm:leading-relaxed">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-        tenetur fuga ducimus numquam ea!
+      <p className="mt-4 max-w-lg sm:text-xl sm:leading-relaxed">
+      With the global chip shortages continuing, your dealership may not be receiving as many new cars as you'd like. This makes it increasingly difficult to reach sales targets. However, you can combat the shortage of new cars by shifting some of your focus to the used car market. If you sell more used cars (and more cars in general), you will be much more likely to receive higher allocation levels from your manufacturer.
       </p>
-
-      <div class="mt-8 flex flex-wrap gap-4 text-center">
-        <Link
-          href="#"
-          class="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
-        >
-          Get Started
-        </Link>
-
-        
-      </div>
     </div>
   </div>
 </section>
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import UniqeCarSection from '../UniqeCarSection/UniqeCarSection';
 
 const Home = () => {
     return (
         <div>
             
             <Banner></Banner>
+            <UniqeCarSection></UniqeCarSection>
         </div>
     );
 };
